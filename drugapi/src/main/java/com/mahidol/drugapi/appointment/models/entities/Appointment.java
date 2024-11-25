a75d8f083351294f0542309435a5ba66a9422453
Kobkit Ruangsuriyakij <kobkit.zhun@gmail.com>
2024-11-25 22:53:36 +0700

    Add appointment table and model

---
+++ drugapi/src/main/java/com/mahidol/drugapi/appointment/models/entities/Appointment.java
@@ -0,0 +1,41 @@
+package com.mahidol.drugapi.appointment.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@Table(name = "appointment")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", columnDefinition = "UUID")
+    private UUID userId;
+
+    @Column(name = "title", columnDefinition = "TEXT")
+    private String title;
+
+    @Column(name = "medic_name", columnDefinition = "TEXT")
+    private String medicName;
+
+    @Column(name = "date", columnDefinition = "DATE")
+    private String date;
+
+    @Column(name = "time", columnDefinition = "TIME")
+    private String time;
+
+    @Column(name = "destination", columnDefinition = "TEXT")
+    private String destination;
+
+    @Column(name = "remark", columnDefinition = "TEXT")
+    private String remark;
+}
