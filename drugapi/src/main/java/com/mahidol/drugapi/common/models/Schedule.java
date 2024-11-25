615e98894f3e1a3eb84517762006db9336a8d65b
Palarp Wasuwat <palarp.was@student.mahidol.edu>
2024-11-25 23:02:09 +0700

    Remove: id in schedule model

--- drugapi/src/main/java/com/mahidol/drugapi/common/models/Schedule.java
+++ drugapi/src/main/java/com/mahidol/drugapi/common/models/Schedule.java
@@ -4,16 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-
 import java.time.LocalTime;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Accessors(chain = true)
 public class Schedule {
-    private UUID id;
     private LocalTime time;
     private Boolean isEnabled = true;
 }
