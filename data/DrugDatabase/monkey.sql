0849c3fd79d98c2a524155d6215180dd644c0e89
zhunismp <kobkit.zhun@gmail.com>
2025-01-19 18:14:21 +0700

    Edit sample data

--- data/DrugDatabase/monkey.sql
+++ data/DrugDatabase/monkey.sql
@@ -22,11 +22,11 @@ INSERT INTO DRUG_GROUP (id, user_id, group_name, drug_id) VALUES (
 -- Insert schedules for individual drug and drug group
 -- For schedule_time, there is timestamp with dummy date. Need to resolved gorm issue with TIME.
 INSERT INTO SCHEDULE (id, user_id, schedule_time, type, name, reference_id, is_enabled) VALUES 
-('1d2f7a4c-ccbc-4ef8-9296-8db0de287831', 'a6f730d8-8f72-4a9f-bf9c-5a6f9f4b7d68', '2000-01-01 17:16:00', 0, 'Morning Dose', 'b2876821-1f75-4c1a-999a-46f8f03d7a4b', TRUE),
-('2e4c5a5d-d72a-44a9-bd47-b64a64d97e9c', 'a6f730d8-8f72-4a9f-bf9c-5a6f9f4b7d68', '2000-01-01 18:10:00', 1, 'Painkillers Group Dose', 'c362f634-e49f-44e9-bbe7-49d94488a6ff', TRUE);
+('1d2f7a4c-ccbc-4ef8-9296-8db0de287831', 'a6f730d8-8f72-4a9f-bf9c-5a6f9f4b7d68', '2000-01-01 17:15:00', 0, 'Morning Dose', 'b2876821-1f75-4c1a-999a-46f8f03d7a4b', TRUE),
+('2e4c5a5d-d72a-44a9-bd47-b64a64d97e9c', 'a6f730d8-8f72-4a9f-bf9c-5a6f9f4b7d68', '2000-01-01 18:00:00', 1, 'Painkillers Group Dose', 'c362f634-e49f-44e9-bbe7-49d94488a6ff', TRUE);
 
 -- Insert history for individual drug
 INSERT INTO HISTORY (id, user_id, drug_id, group_id, status, taken_at, notified_at, count) VALUES (
     '9c77574b-b1ab-49a4-8bb3-bddff16718da', 'a6f730d8-8f72-4a9f-bf9c-5a6f9f4b7d68', 
-    'b2876821-1f75-4c1a-999a-46f8f03d7a4b', NULL, 'taken', '2025-01-18 09:00:00', '2025-01-18 08:55:00', 1
+    'b2876821-1f75-4c1a-999a-46f8f03d7a4b', NULL, 'taken', '2025-01-18 09:00:00', '2025-01-18 08:45:00', 1
 );
