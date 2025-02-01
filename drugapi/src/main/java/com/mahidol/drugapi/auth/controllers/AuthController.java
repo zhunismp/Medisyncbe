b7738bd991f8d3409b7f11cb9f36b0771da03da8
Kobkit Ruangsuriyakij <96523298+zhunismp@users.noreply.github.com>
2025-02-01 21:37:12 +0700

    [feat] Add isRegistered flag when user login (#29)

--- drugapi/src/main/java/com/mahidol/drugapi/auth/controllers/AuthController.java
+++ drugapi/src/main/java/com/mahidol/drugapi/auth/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.mahidol.drugapi.auth.controllers;
 import com.mahidol.drugapi.auth.dtos.GoogleAuthRequest;
 import com.mahidol.drugapi.auth.services.GoogleAuthService;
 import com.mahidol.drugapi.common.utils.JWTUtil;
+import com.mahidol.drugapi.user.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +17,16 @@ import java.util.UUID;
 @RequestMapping("/api/v1")
 public class AuthController {
     private final GoogleAuthService googleAuthService;
+    private final UserService userService;
     private final JWTUtil jwtUtil;
 
     public AuthController(
             GoogleAuthService googleAuthService,
+            UserService userService,
             JWTUtil jwtUtil
     ) {
         this.googleAuthService = googleAuthService;
+        this.userService = userService;
         this.jwtUtil = jwtUtil;
     }
 
@@ -30,6 +34,7 @@ public class AuthController {
     public ResponseEntity<?> googleAuth(@RequestBody GoogleAuthRequest request) {
         UUID userId = googleAuthService.verify(request.getIdToken());
         String token = jwtUtil.generateJWT(userId);
-        return ResponseEntity.ok(Map.of("token", token));
+
+        return ResponseEntity.ok(Map.of("token", token, "isRegistered", userService.isExists(userId)));
     }
 }
