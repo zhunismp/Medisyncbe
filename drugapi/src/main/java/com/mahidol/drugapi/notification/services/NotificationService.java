af024b58b0e8bc7b73fae8a9fbee82eb8afcc76d
zhunismp <kobkit.zhun@gmail.com>
2025-02-15 21:38:38 +0700

    remove unused module

--- drugapi/src/main/java/com/mahidol/drugapi/notification/services/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.mahidol.drugapi.notification.services;
-
-import com.mahidol.drugapi.notification.models.NotificationMessage;
-
-public interface NotificationService {
-    void send(NotificationMessage message);
-}
